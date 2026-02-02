eeb6014770290c45bc461cde4148c1966c783b8e
Krypton <root@krypton.ninja>
2026-02-02 19:11:15 +0100

    feat: Add useful headers in infrastructure module

--- src/modules/infrastructure/mod.rs
+++ src/modules/infrastructure/mod.rs
@@ -1,14 +1,28 @@
+use std::collections::HashMap;
+use std::vec;
+
 use crate::database::node::Type;
 use crate::event_bus::Event;
 use crate::logger;
 use crate::modules::Module;
 use crate::session::Session;
 
-pub struct ModuleInfrastructure;
+pub struct ModuleInfrastructure {
+    headers_list: Vec<&'static str>,
+}
 
 impl ModuleInfrastructure {
     pub fn new() -> Self {
-        ModuleInfrastructure
+        ModuleInfrastructure {
+            headers_list: vec![
+                "server",
+                "x-powered-by",
+                "cf-ray",
+                "authorization",
+                "set-cookie",
+                "last-modified",
+            ],
+        }
     }
 }
 
@@ -34,10 +48,9 @@ impl Module for ModuleInfrastructure {
         };
         let domain = &fetched_data.domain;
         let tls = &fetched_data.response.tls;
+        let headers = &fetched_data.response.headers;
 
         if let Some(tls_data) = &tls {
-            logger::println(self.name(), format!("Gathered TLS data for {}", domain));
-
             for san in &tls_data.san {
                 if !session.get_state().has_discovered_domain(san.to_string()) {
                     session.publish(Event::DiscoveredDomain(san.clone()));
@@ -49,7 +62,25 @@ impl Module for ModuleInfrastructure {
                 .search(Type::Domain, domain.to_string())
             {
                 parent.add_data("tls".to_string(), serde_json::to_value(tls_data).unwrap());
+                logger::println(self.name(), format!("Gathered TLS data for {}", domain));
             }
+        }
+
+        let mut useful_headers = HashMap::new();
+        for (name, value) in headers {
+            if self.headers_list.contains(&name.as_str()) {
+                useful_headers.insert(name, value);
+            }
+        }
+        if let Some(parent) = session
+            .get_database()
+            .search(Type::Domain, domain.to_string())
+        {
+            parent.add_data(
+                "headers".to_string(),
+                serde_json::to_value(useful_headers).unwrap(),
+            );
+            logger::println(self.name(), format!("Gathered headers data for {}", domain));
         }
 
         Ok(())
